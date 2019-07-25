530589255b1c2419eefabb34d6045ffa989a0a8c
jennifer <jennifer.malleret@outlook.fr>
2019-07-25 11:50:12 +0200

    exercice 12 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 12</title>
+</head>
+<body>
+  <h1>Afficher les nombre impair de 0 à 300</h1>
+  <?php
+  for ($i = 1; $i < 300 ; $i += 2){
+    echo $i. ' ' ;
+  }
+  ?>
+</body>
+</html>
